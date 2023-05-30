2d257158b8b567f63110d94d7a4c2c256a8e52f4
Michael .C. Amadi <mgregchi[at]gmail.com>
2023-05-30 10:21:04 +0100

    remove trailig space on  module doc to 0x16-api/100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
- Write a recursive function that queries the Reddit API,
- parses the title of all hot articles, and prints a sorted
- count of given keywords (case-insensitive, delimited by spaces.
- Javascript should count as javascript, but java should not).
+Write a recursive function that queries the Reddit API,
+parses the title of all hot articles, and prints a sorted
+count of given keywords (case-insensitive, delimited by spaces.
+Javascript should count as javascript, but java should not).
 """
-
 import requests
+
 
 def count_words(subreddit, word_list, after=None, count={}):
     """a recursive function that queries the Reddit API,
