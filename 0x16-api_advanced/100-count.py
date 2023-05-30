c87d2925cb5b014ee64f8cc265baddcc5591ebcd
Michael .C. Amadi <mgregchi[at]gmail.com>
2023-05-30 10:22:41 +0100

    remove trailig space on  module doc to 0x16-api/100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """
-Write a recursive function that queries the Reddit API,
-parses the title of all hot articles, and prints a sorted
-count of given keywords (case-insensitive, delimited by spaces.
-Javascript should count as javascript, but java should not).
+a recursive function that queries the Reddit API
 """
 import requests
 
