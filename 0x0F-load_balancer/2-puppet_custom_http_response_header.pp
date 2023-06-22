7ae26a8e48af6c3fe5d821bd18340e2fbf287c2c
Michael .C. Amadi <mgregchi[at]gmail.com>
2023-06-22 21:34:39 +0100

    Fix puppet nginx install not working

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,43 +2,44 @@
 
 # Install Nginx package
 package { 'nginx':
+  ensure   => installed,
   provider => 'apt',
 }
 
 # Create index.nginx-debian.html file with "Hello World!" content
-exec { 'home_page':
-  command => '/usr/bin/sudo /bin/echo Hello World! > /var/www/html/index.nginx-debian.html',
+file { '/var/www/html/index.nginx-debian.html':
+  content => 'Hello World!',
+  require => Package['nginx'],
 }
 
-# Configure redirection to github for /redirect_me path
-$server_config = "server {
-      listen 80;
-      listen [::]:80 default_server;
-      root   /var/www/html/;
-      index  index.html;
+# Configure redirection to GitHub for /redirect_me path
+file { '/etc/nginx/sites-available/default':
+  content => "server {
+    listen 80;
+    listen [::]:80 default_server;
+    root   /var/www/html/;
+    index  index.html;
 
-      # Redirect /redirect_me path with a 301 status code
-      location /redirect_me {
-          return 301 https://github.com/mgregchi permanent;
-      }
+    # Redirect /redirect_me path with a 301 status code
+    location /redirect_me {
+        return 301 https://github.com/mgregchi permanent;
+    }
 
-      error_page 404 /custom_404.html;
+    error_page 404 /custom_404.html;
 
-      location = /custom_404.html {
-          root /var/www/html;
-          internal;
-      }
+    location = /custom_404.html {
+        root /var/www/html;
+        internal;
+    }
 
-      add_header X-Served-By $hostname;
-  }
-"
-# Add custom config
-exec { 'custom_http_header':
-  command => '/usr/bin/echo /bin/echo ${server_config} > /etc/nginx/sites-available/default',
-
+    add_header X-Served-By $hostname;
+  }",
+  require => Package['nginx'],
+  notify  => Exec['restart_nginx'],
 }
 
-# Start Nginx service
-exec { 'start_server':
-  command => '/usr/bin/sudo /usr/sbin/service nginx start',
+# Restart Nginx service
+exec { 'restart_nginx':
+  command     => '/usr/sbin/service nginx restart',
+  refreshonly => true,
 }
