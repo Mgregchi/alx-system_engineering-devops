cf9c499557e5e0f83bb1d6fba1a11561b76633ec
Michael .C. Amadi <mgregchi[at]gmail.com>
2023-06-22 20:29:28 +0100

    Fix puppet not working

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,53 +1,44 @@
-# Install Nginx web server and create HTML pages
-
-# Update package lists
-exec { 'update-package-lists':
-  command => 'apt-get update',
-  path    => '/usr/bin',
-}
+# Puppet manifest to install and configure Nginx on Ubuntu
 
 # Install Nginx package
 package { 'nginx':
-  ensure  => installed,
-  require => Exec['update-package-lists'],
+  provider => 'apt',
 }
 
-# Create index.html file with "Hello World!" content
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-  require => Package['nginx'],
+# Create index.nginx-debian.html file with "Hello World!" content
+exec { 'home_page':
+  command => '/usr/bin/sudo /bin/echo Hello World! > /var/www/html/index.nginx-debian.html',
 }
 
-# Configure Nginx default site
-file { '/etc/nginx/sites-available/default':
-  content => "
-    server {
-        listen 80;
-        listen [::]:80 default_server;
-        root   /var/www/html/;
-        index  index.html;
+# Configure redirection to github for /redirect_me path
+$server_config = "server {
+      listen 80;
+      listen [::]:80 default_server;
+      root   /var/www/html/;
+      index  index.html;
 
-        # Redirect /redirect_me path with a 301 status code
-        location /redirect_me {
-            return 301 https://github.com/mgregchi;
-        }
+      # Redirect /redirect_me path with a 301 status code
+      location /redirect_me {
+          return 301 https://github.com/mgregchi permanent;
+      }
 
-        error_page 404 /custom_404.html;
+      error_page 404 /custom_404.html;
 
-        location = /custom_404.html {
-            root /var/www/html;
-            internal;
-        }
+      location = /custom_404.html {
+          root /var/www/html;
+          internal;
+      }
 
-        add_header X-Served-By $hostname;
-    }
-  ",
-  require => Package['nginx'],
+      add_header X-Served-By $hostname;
+  }
+"
+# Add custom config
+exec { 'custom_http_header':
+  command => '/usr/bin/echo /bin/echo ${server_config} > /etc/nginx/sites-available/default',
+
 }
 
-# Restart Nginx service
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => [File['/etc/nginx/sites-available/default']],
+# Start Nginx service
+exec { 'start_server':
+  command => '/usr/bin/sudo /usr/sbin/service nginx start',
 }
